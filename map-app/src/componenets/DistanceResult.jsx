a48e8bc305ea2ca3b37a7aebec072456b1761e65
Kgiruc <kgiruc76@gmail.com>
2023-02-26 01:29:49 +0100

    pdf print

--- map-app/src/componenets/DistanceResult.jsx
+++ map-app/src/componenets/DistanceResult.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { useRef } from 'react';
+import { useReactToPrint } from 'react-to-print';
 
 
 function DistanceResult({ distance }) {
+    const componentRef = useRef();
+    const handlePrint = useReactToPrint({
+        content: () => componentRef.current,
+        documentTitle: `${distance.origin_addresses}`,
+        onAfterPrint: () => alert("Print success")
+    })
 
 
     const kilometrowka = 1.2
@@ -10,16 +17,25 @@ function DistanceResult({ distance }) {
     const all = Math.round((wynikdnia + (dystans * kilometrowka)))
 
     return (
-        <div className='bg-green-400'>
-            <ul>
-                <li>{distance.origin_addresses}</li>
-                <li>{distance.destination_addresses}</li>
-                <li>dystans: {dystans}km</li>
-                <li>1000 zł za dzień: {wynikdnia}</li>
-                <li>bez podatku: {all}</li>
-                <li>z podatkiem :{all * 10 / 100 + all}zł</li>
-            </ul>
-        </div>
+        <>
+            <div className='bg-green-400' ref={componentRef}>
+                <ul>
+                    <li>{distance.origin_addresses}</li>
+                    <li>{distance.destination_addresses}</li>
+                    <li>dystans: {dystans}km</li>
+                    <li>stały mnożnik: {kilometrowka}km</li>
+                    <li>1000 zł za dzień x {wynikdnia} dni</li>
+                    <li>bez podatku: {all}</li>
+                    <li>z podatkiem :{all * 10 / 100 + all}zł</li>
+                </ul>
+                <button
+                className='border-2 border-black bg-blue-200' 
+                onClick={handlePrint}>
+                Print pdf
+            </button>
+            </div>
+            
+        </>
     )
 }
 
--- map-app/src/componenets/Map.jsx
+++ map-app/src/componenets/Map.jsx
@@ -8,7 +8,7 @@ import MatrixDistance from "./MatrixDistance";
 function Map() {
     const position = [51.505, -0.09]
     return (
-        <div>
+        <div className="flex flex-row">
         <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
